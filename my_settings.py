77b7fd23fdd219608a4e44cde0ba4530523a3c60
박준영 <sjwm98@gmail.com>
2021-05-23 15:23:58 +0900

    CREATE: 프로젝트 초기 셋팅

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+SECRET_KEY = 'django-insecure-b6n5q1%_mw*=6(i_=k$d8$8ma_)1v4%c02!dxm((v_octhp8%s'
+
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westarbucks1',
+        'USER': 'root',
+        'PASSWORD': 'go9511455',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
---
+++ westarbucks1/models.py
@@ -0,0 +1,57 @@
+from typing import Sized
+from django.db import models
+from django import db
+from django.db.models.aggregates import Max
+# Create your models here.
+class Menu(models.Model):
+    name = models.CharField(max_length=45)
+    class Meta:
+        db_table='menu'
+
+class Category(models.Model):
+    name = models.CharField(max_length=45)
+    menu = models.ForeignKey('Menu', on_delete=models.CASCADE)
+    class Meta:
+        db_table='categories'
+
+class Nutrition(models.Model):
+    one_serving_kcal = models.DecimalField(max_digits=6, decimal_places=2)
+    sodium_mg = models.DecimalField(max_digits=6, decimal_places=2)
+    saturated_fat_g = models.DecimalField(max_digits=6, decimal_places=2)
+    sugars_g = models.DecimalField(max_digits=6, decimal_places=2)
+    protein_g = models.DecimalField(max_digits=6, decimal_places=2)
+    caffeine_mg = models.DecimalField(max_digits=6, decimal_places=2)
+    size_ml = models.CharField(max_length=45)
+    size_fluid_ounce = models.CharField(max_length=45)
+    class Meta:
+        db_table='nutritions'
+
+class Product(models.Model):
+    korean_name = models.CharField(max_length=45)
+    english_name = models.CharField(max_length=45)
+    description = models.TextField()
+    nutritions = models.ForeignKey(Nutrition, on_delete=models.CASCADE)
+    categories = models.ForeignKey(Category,  on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table='products'
+
+class Image(models.Model):
+    image_url = models.CharField(max_length=45)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table='images'
+
+class Allergy(models.Model):
+    name = models.CharField(max_length=45)
+
+    class Meta:
+        db_table='allergy'
+
+class Allergy_product(models.Model):
+    allergy = models.ForeignKey(Allergy, on_delete=models.CASCADE)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table='allergy_products'
